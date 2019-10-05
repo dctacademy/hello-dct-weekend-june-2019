e731a17ffb17377624fbe253b77480d0a6627875
Nandish <nandishp.reddy@gmail.com>
2019-10-05 14:04:00 +0530

    priyanka.js edited

--- priyanka.js
+++ priyanka.js
@@ -1,1 +1,18 @@
-// This is created by priyanka
+const http = require('http')
+const port = 3010
+
+const server = http.createServer(function(request,response){
+    if(request.url == '/'){
+        response.end('response from node server')
+    }
+    else if(request.url == '/about') {
+        response.end('about page')
+    }
+    else {
+        response.end('The page you are looking for does not exist')
+    }
+})
+
+server.listen(port, function(){
+ console.log('listening on port',port)
+})
